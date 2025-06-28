314c17ed19170d20c71cce1d42cb4aa433fd5e98
Artem <75899192+Chel0vek690@users.noreply.github.com>
2025-06-29 00:23:48 +0400

    feat: изменение background у кнопки при переключении по слайдам

--- Union of Volunteers/Union of Volunteers/Models/AboutSlide.cs
+++ Union of Volunteers/Union of Volunteers/Models/AboutSlide.cs
@@ -8,6 +8,7 @@ namespace Union_of_Volunteers.ViewModels.Pages
 {
     public class AboutSlide
     {
+        public int? Id {  get; set; }
         public string? Title { get; set; }
         public string? Image { get; set; }
     }
--- Union of Volunteers/Union of Volunteers/ViewModels/Pages/AboutPageViewModel.cs
+++ Union of Volunteers/Union of Volunteers/ViewModels/Pages/AboutPageViewModel.cs
@@ -13,16 +13,22 @@ namespace Union_of_Volunteers.ViewModels.Pages
         [ObservableProperty]
         private int currentSlideIndex;
 
+        [ObservableProperty]
+        private string backgroundButtonRight = "#12529E";
+
+        [ObservableProperty]
+        private string backgroundButtonLeft = "#3312529E";
+
         public AboutSlide? CurrentSlide => Slides.Count > 0 ? Slides[CurrentSlideIndex] : null;
 
         public AboutPageViewModel(NavigationService<MainPageViewModel> mainNavigationService)
         {
             _mainNavigationService = mainNavigationService;
-            Slides.Add(new AboutSlide { Title = "Об организации", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization1.png" });
-            Slides.Add(new AboutSlide { Title = "Об организации", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization2.png" });
-            Slides.Add(new AboutSlide { Title = "Об организации", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization3.png" });
-            Slides.Add(new AboutSlide { Title = "Наша команда", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization4.png" });
-            Slides.Add(new AboutSlide { Title = "Наша команда", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization5.png" });
+            Slides.Add(new AboutSlide { Id = 1, Title = "Об организации", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization1.png" });
+            Slides.Add(new AboutSlide { Id = 2, Title = "Об организации", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization2.png" });
+            Slides.Add(new AboutSlide { Id = 3, Title = "Об организации", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization3.png" });
+            Slides.Add(new AboutSlide { Id = 4, Title = "Наша команда", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization4.png" });
+            Slides.Add(new AboutSlide { Id = 5, Title = "Наша команда", Image = "pack://application:,,,/;component/Resources/Images/AboutOrganization5.png" });
         }
 
         [RelayCommand]
@@ -30,6 +36,8 @@ namespace Union_of_Volunteers.ViewModels.Pages
         {
             if (CurrentSlideIndex < Slides.Count - 1)
             {
+                if (CurrentSlide.Id == 4) BackgroundButtonRight = "#3312529E";
+                else BackgroundButtonLeft = "#12529E";
                 CurrentSlideIndex++;
                 OnPropertyChanged(nameof(CurrentSlide));
             }
@@ -38,6 +46,8 @@ namespace Union_of_Volunteers.ViewModels.Pages
         [RelayCommand]
         private void PrevSlide()
         {
+            if (CurrentSlide.Id == 2) BackgroundButtonLeft = "#3312529E";
+            else BackgroundButtonRight = "#12529E";
             if (CurrentSlideIndex > 0)
             {
                 CurrentSlideIndex--;
